e34ef47fd422ec46284b63dd240af12651b5723f
Edgar Nazaryan <edgarnazaryancisco@gmail.com>
2023-08-01 21:57:07 +0400

    lesson8 tco classroom second commit

--- src/Components/Home/Details/CoctailsDetail.js
+++ src/Components/Home/Details/CoctailsDetail.js
@@ -1,20 +1,22 @@
 import { Link } from "react-router-dom";
-
+import "./style.css"
 const CoctailsDetail = ({idDrink, strDrink, strDrinkThumb, strCategory, strAlcoholic, strGlass, strInstructions}) => {
     return (
         <div key={idDrink}>
             <Link to='/'>Back Home</Link>
             <h2>{strDrink}</h2>
-            <>
-                <img src={strDrinkThumb}/>
-            </>
-            <>
-                <p>Name: {strDrink}</p>
-                <p>Category: {strCategory}</p>
-                <p>Info: {strAlcoholic}</p>
-                <p>Glass: {strGlass}</p>
-                <p>Instructions: {strInstructions}</p>
-            </>
+            <div className="Detail">
+                <>
+                    <img src={strDrinkThumb}/>
+                </>
+                <div className="description">
+                    <p>Name: {strDrink}</p>
+                    <p>Category: {strCategory}</p>
+                    <p>Info: {strAlcoholic}</p>
+                    <p>Glass: {strGlass}</p>
+                    <p>Instructions: {strInstructions}</p>
+                </div>
+            </div>
         </div>
         // <>Babken
         // </>
