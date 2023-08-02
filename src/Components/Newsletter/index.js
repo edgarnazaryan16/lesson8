9319db824d58df1d15355691752c281c3d102cd0
unknown <edgarnazaryancisco@gmail.com>
2023-08-02 13:56:24 +0400

    lesson8 forexam commit

--- src/Components/Newsletter/index.js
+++ src/Components/Newsletter/index.js
@@ -3,26 +3,33 @@ import "./style.css"
 const { useState } = require("react")
 
 const NewsLetter = () => {
-    const [inputName, setInputName] = useState('');
-    const [inputLastName, setInputLastName] = useState('');
-    const [inputEmail, setInputEmail] = useState('');
+    // const [inputName, setInputName] = useState('');
+    // const [inputLastName, setInputLastName] = useState('');
+    // const [inputEmail, setInputEmail] = useState('');
 
-    const handleInputNameChange = event => {
-        setInputName(event.target.value)
+    const [info, setInfo] = useState({
+        Name: '',
+        Lastname: '',
+        Email: ''
+    })
+
+    // const handleInputNameChange = event => {
+    //     setInputName(event.target.value)
+    // }
+    // const handleInputLastNameChange = event => {
+    //     setInputLastName(event.target.value)
+    // }
+    // const handleInputEmailChange = event => {
+    //     setInputEmail(event.target.value)
+    // }
+
+    const handleFormChange = (event, field) => {
+        setInfo({...info, [field]: event.target.value})
     }
-    const handleInputLastNameChange = event => {
-        setInputLastName(event.target.value)
-    }
-    const handleInputEmailChange = event => {
-        setInputEmail(event.target.value)
-    }
+
     const handleSubmit = (event) => {
         event.preventDefault()
-        localStorage.setItem("user", JSON.stringify({
-            Name: inputName,
-            LastName: inputLastName,
-            Email: inputEmail
-        }))
+        localStorage.setItem("user", JSON.stringify(info))
     }
     return (
         <>
@@ -31,11 +38,11 @@ const NewsLetter = () => {
                 <h2>Our Newsletter</h2>
                 <form onSubmit={handleSubmit}>
                     <label>Name</label>
-                    <input type="text" value={inputName} onChange={handleInputNameChange}/>
+                    <input type="text" value={info.Name} onChange={(event) => handleFormChange(event, 'Name')}/>
                     <label>Last Name</label>
-                    <input type="text" value={inputLastName} onChange={handleInputLastNameChange}/>
+                    <input type="text" value={info.Lastname} onChange={(event) => handleFormChange(event, 'Lastname')}/>
                     <label>Email</label>
-                    <input type="text" value={inputEmail} onChange={handleInputEmailChange}/>
+                    <input type="text" value={info.Email} onChange={(event) => handleFormChange(event, 'Email')}/>
                     <button>Submit</button>
                 </form>
             </div>
