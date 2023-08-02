f60f8e145943bf5aa035e84ed9e33dad228cfa7f
unknown <edgarnazaryancisco@gmail.com>
2023-08-02 14:05:52 +0400

    02.08 commit

--- src/Components/Newsletter/index.js
+++ src/Components/Newsletter/index.js
@@ -3,25 +3,12 @@ import "./style.css"
 const { useState } = require("react")
 
 const NewsLetter = () => {
-    // const [inputName, setInputName] = useState('');
-    // const [inputLastName, setInputLastName] = useState('');
-    // const [inputEmail, setInputEmail] = useState('');
 
     const [info, setInfo] = useState({
         Name: '',
         Lastname: '',
         Email: ''
     })
-
-    // const handleInputNameChange = event => {
-    //     setInputName(event.target.value)
-    // }
-    // const handleInputLastNameChange = event => {
-    //     setInputLastName(event.target.value)
-    // }
-    // const handleInputEmailChange = event => {
-    //     setInputEmail(event.target.value)
-    // }
 
     const handleFormChange = (event, field) => {
         setInfo({...info, [field]: event.target.value})
